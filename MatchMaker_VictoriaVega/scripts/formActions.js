fb418c6da2c0e4312b2f132bf656fdc46d5cc904
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2024-12-03 17:24:22 -0700

    Práctica FetchApi

--- MatchMaker_VictoriaVega/scripts/formActions.js
+++ MatchMaker_VictoriaVega/scripts/formActions.js
@@ -40,7 +40,8 @@ window.onload = function() {
     }
 
     llenarNacionalidad();
-    form[0].onsubmit = enviar;
+    //form[0].onsubmit = enviar;
+    form[0].onsubmit = enviar2;
 }
 
 function llenarNacionalidad() {
@@ -69,3 +70,61 @@ function resaltarDesresaltar(evento) {
     evento.target.classList.toggle("selected");
 }
 
+function obtenerGenero() {
+    /*
+    const gSelected = document.querySelectorAll("input[name=interest]:checked");
+    
+    for(let g of gSelected) {
+        activos.push(g.value);
+    }
+    */
+    
+    const genero = document.getElementsByName("interest");
+    const activos = [];
+    for(let g of genero) {
+        if (g.checked) {
+            activos.push(g.value);
+        }
+    }
+    if (activos.length == 0) {
+        activos.push("male");
+        activos.push("female");
+    }
+
+    const index = activos.length > 1 ? Math.floor(Math.random() * 2) : 0;
+    return activos[index];
+}
+
+function enviar(evento) {
+    evento.preventDefault();
+    const nacionalidad = document.getElementById("nationality");
+    const genero = obtenerGenero();
+
+    window.location.href = `its-a-match.html?nac=${nacionalidad.value}&genero=${genero}`;
+}
+
+function enviar2(evento) {
+    evento.preventDefault();
+
+    const nac = document.getElementById("nationality");
+    const genero = obtenerGenero();
+
+    const request = new Request(
+        `https://randomuser.me/api/?nat=${nac}&gender=${genero}`, 
+        {
+            method: 'get',
+            headers: new Headers({
+                "Content-Type": "application/json"
+            })
+        }
+    );
+
+    fetch(request).then(function(response){
+        return response.json();
+    }).then(function(data){
+        localStorage.setItem("matchInfo", JSON.stringify(data.results[0]));
+        window.location.href = "its-a-match.html";
+    }).catch(function(error){
+        alert("Hubo un error encontrando a tu otra mitad :(");
+    })
+}
